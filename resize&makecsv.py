643058772a305d30a195c56f0f75545a2e7367d9
apzzd <59454160+apzzd@users.noreply.github.com>
2021-07-26 18:31:53 -0400

    commit

--- resize&makecsv.py
+++ resize&makecsv.py
@@ -6,11 +6,11 @@ directory = "photosForData"
 
 with open('example.csv', 'w', encoding='UTF8', newline='') as x:
     writer = csv.writer(x)
-    writer.writerow(["Latitude", "Longitude", "Tags", "Comment", "Image"])
+    writer.writerow(["Latitude", "Longitude", "Tags", "Comment", "Filename", "Image"])
     for filename in os.listdir(directory):
         f = os.path.join(directory, filename)
         data = gpsphoto.getGPSData(f)
         print(f)
         image = "<img src='https://raw.githubusercontent.com/apzzd/Dangerous_Places/main/photos/" + filename +"'>"
-        writer.writerow([data["Latitude"], data["Longitude"], "[tag]", "[comment]", image])
+        writer.writerow([data["Latitude"], data["Longitude"], "[tag]", "[comment]", filename,image])
     
