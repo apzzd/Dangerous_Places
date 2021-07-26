fbd9e3484e2e891cad2cbf9bb901084e59f998ca
apzzd <59454160+apzzd@users.noreply.github.com>
2021-07-26 17:44:18 -0400

    update csv making program

--- get_photo_info.py
+++ get_photo_info.py
@@ -5,9 +5,9 @@ directory = "photos"
 
 with open('example.csv', 'w', encoding='UTF8', newline='') as x:
     writer = csv.writer(x)
-    writer.writerow(["Latitude", "Longitude", "Filename"])
+    writer.writerow(["Latitude", "Longitude", "Tags", "Comment", "Image"])
     for filename in os.listdir(directory):
         f = os.path.join(directory, filename)
         data = gpsphoto.getGPSData(f)
-        writer.writerow([data["Latitude"], data["Longitude"], filename])
+        writer.writerow([data["Latitude"], data["Longitude"], "", "", "<img src='https://raw.githubusercontent.com/apzzd/Dangerous_Places/main/photos/" + filename +"'>"])
     
