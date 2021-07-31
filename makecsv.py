3716bd6592c0846d91a83e271d9f2ae5d3c1e54d
apzzd <59454160+apzzd@users.noreply.github.com>
2021-07-31 14:59:50 -0400

    committing

--- makecsv.py
+++ makecsv.py
@@ -1,16 +1,16 @@
 from GPSPhoto import gpsphoto
 import csv, os
-directory = "photosForData"
+directory = "newPhotos"
 
 
 
-with open('example.csv', 'w', encoding='UTF8', newline='') as x:
+with open('data2.csv', 'w', encoding='UTF8', newline='') as x:
     writer = csv.writer(x)
     writer.writerow(["Latitude", "Longitude", "Tags", "Comment", "Image"])
     for filename in os.listdir(directory):
         f = os.path.join(directory, filename)
-        data = gpsphoto.getGPSData(f)
+        data = gpsphoto.getGPSData("/newPhotos/IMG_1179.jpeg")
         print(f)
-        image = "<img src='https://raw.githubusercontent.com/apzzd/Dangerous_Places/main/photos/" + filename +"'>"
+        image = "<img src='https://raw.githubusercontent.com/apzzd/Dangerous_Places/main/otherPhotos/" + filename +"'>"
         writer.writerow([data["Latitude"], data["Longitude"], "[tag]", "[comment]", image])
     
